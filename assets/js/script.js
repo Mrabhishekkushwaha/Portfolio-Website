36924453236b303d607cd6c003d8e060225d3a41
Mrabhishekkushwaha <160748125+Mrabhishekkushwaha@users.noreply.github.com>
2024-03-11 19:43:19 +0530

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,9 +64,9 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Anoop Singh";
+            document.title = "Portfolio | Abhishek Kushwaha";
             $("#favicon").attr("href", "assets/images/favicon.png");
-        }
+        
         else {
             document.title = "Come Back To Portfolio";
             $("#favicon").attr("href", "assets/images/favhand.png");
